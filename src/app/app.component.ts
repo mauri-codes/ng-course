fa5c4af6e6973fba76650f67f091d30028927542
Mauricio-Alarcon <Mauricio.Alarcon@coxautoinc.com>
2017-12-04 23:20:06 -0300

    data binding class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
+  color = 'red';
+  side = 300;
+  colors = ['yellow', 'red', 'papayawhip', 'black', 'blue'];
+  index = 0;
 }
