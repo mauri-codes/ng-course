398ba6cf64620de40cdf91181feb404f4768b1a6
Mauricio Alarcon <mauricio.alarcon@coxautoinc.com>
2017-12-12 19:13:24 -0400

    ordered folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AdminComponent } from "./admin/admin.component";
-import { DetailComponent } from "./admin/detail/detail.component";
+import { AdminComponent } from "./view/admin/admin.component";
+import { DetailComponent } from "./view/admin/detail/detail.component";
 
 @NgModule({
   declarations: [
