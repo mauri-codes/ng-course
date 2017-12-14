99e51e813844fda53473e23670874d2fef61eced
Mauricio Alarcon <mauricio.alarcon@coxautoinc.com>
2017-12-14 18:52:39 -0400

    added app-routing.module.ts

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from "./view/admin/admin.component";
+
+const routes: Routes = [
+  { path: 'admin', component: AdminComponent}
+];
+
+@NgModule({
+  exports: [ RouterModule ],
+  imports: [ RouterModule.forRoot(routes) ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from "./app-routing.module";
 
 import { AppComponent } from './app.component';
 import { AdminComponent } from "./view/admin/admin.component";
@@ -17,7 +18,8 @@ import { MoviesService } from "./services/movies.service";
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [
     MoviesService
