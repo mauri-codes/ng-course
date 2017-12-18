12f9f65a4672b3ef40fe5f638ed5168c84145927
Mauricio-Alarcon <Mauricio.Alarcon@coxautoinc.com>
2017-12-18 09:59:37 -0300

    intermediate branch

--- src/app/view/movies/movies.component.ts
+++ src/app/view/movies/movies.component.ts
@@ -1,10 +1,47 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { MoviesService } from "../../services/movies.service";
+import { DomSanitizer, SafeUrl} from "@angular/platform-browser";
 
 @Component({
   selector: "app-movies",
   styleUrls: ["movies.component.scss"],
   templateUrl: "movies.component.html"
 })
-export class MoviesComponent {
+export class MoviesComponent implements OnInit {
   title = "Movies page";
+  id: number;
+  movie;
+  movies;
+  constructor(private route: ActivatedRoute,
+              private movieService: MoviesService,
+              private sanitazer: DomSanitizer) {}
+  ngOnInit() {
+    const stringId = this.route.snapshot.paramMap.get('id');
+    let id = -1;
+    if ( stringId != null) {
+      id = +stringId;
+      this.getMovie(id);
+    } else {
+      this.getMovies();
+    }
+    this.id = id;
+  }
+  getMovie(id) {
+    this.movieService
+      .getMovie(id)
+      .subscribe(movie => {
+        this.movie = movie;
+      });
+  }
+  getMovies() {
+    this.movieService
+      .getMovies()
+      .subscribe(movies => {
+        this.movies = movies;
+      });
+  }
+  cleanUrl (url): SafeUrl {
+    return this.sanitazer.bypassSecurityTrustResourceUrl(url);
+  }
 }
